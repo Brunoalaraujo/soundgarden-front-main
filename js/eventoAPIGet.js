a0ba5ceaa24236e365ed07fd871c2708e0a27026
Brunoandradel.a <brunoandradel.a.dev@gmail.com>
2022-07-01 11:28:27 -0300

    Projeto Finalizado

--- js/eventoAPIGet.js
+++ js/eventoAPIGet.js
@@ -14,11 +14,11 @@ function carregarPg() {
             htmlEventos += `
                 <tr>
                   <th scope="row">${i + 1}</th>
-                  <td>${eventos[i].scheduled}</td>
+                  <td>${eventos[i].scheduled.replace(":00.000Z", "")}</td>
                   <td>${eventos[i].name}</td>
                   <td>${eventos[i].attractions}</td>
                   <td>
-                      <a href="reservas.html" class="btn btn-dark">ver reservas</a>
+                      <a href="reservas.html?id=${eventos[i]._id}" class="btn btn-dark">ver reservas</a>
                       <a href="editar-evento.html?id=${eventos[i]._id}" class="btn btn-secondary">editar</a>
                       <a href="excluir-evento.html?id=${eventos[i]._id}" class="btn btn-danger">excluir</a>
                   </td>
